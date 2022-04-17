5762ba1ffa2ec8896afe962704762ffefe3d5947
Marco Alarcon <alarconm96@gmail.com>
2022-04-17 18:48:48 -0400

    Implement isAvailable method

--- src/assignment6/HashTable.java
+++ src/assignment6/HashTable.java
@@ -1,19 +1,31 @@
 package assignment6;
-public class HashTable{
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class HashTable extends ArrayList{
     private static int numElement;
     private Entry DEFUNCT = new Entry(null, null); // sentinel for deleted item
     private int collisions; // counts the total number of collisions during insertion
 
     public HashTable(int capacity){
-      ArrayList<Entry> table = new ArrayList<Entry>(capacity);  
+      ArrayList table = new ArrayList(capacity);  
     }
     
     //Returns true if location is either empty or the defunct sentinel
     private boolean isAvailable(int j) {
-        return false;
+    	checkIndex(j, size);
+    	if (data[j] == null || data[j].equals(DEFUNCT)) {
+			return true;
+		}
+    	return false;
     }
+    
     // compute and return hash code given the key
-    private int computeHashCode(String key){return 0;}
+    private int computeHashCode(String key){
+    	return 0;
+	}
     
     // compute and return the compressed hash index 
     private int compressHashCode(){return 0;}
@@ -30,5 +42,14 @@ public class HashTable{
     //print the content of the table
     public void tablePrint(){}
     
+    //read from file input and return String
+    private String read(String file) throws FileNotFoundException {
+    	Scanner sc = new Scanner(new FileReader("HashTable.csv"));
+    	String input = "";
+    	while (sc.hasNext()) {
+			input+= sc.nextLine() + "\n";
+		}
+    	return input;
+    }
     
 }  
