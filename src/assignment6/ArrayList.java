298b22bdce96c50161b5946a35d14b35274d4dc8
Marco Alarcon <alarconm96@gmail.com>
2022-04-17 18:39:07 -0400

    Remove Entry.java and nest Entry class within ArrayList.java

--- src/assignment6/ArrayList.java
+++ src/assignment6/ArrayList.java
@@ -1,14 +1,19 @@
 package assignment6;
 
-public class ArrayList<E> {
-    protected E[] data;
-    protected int size = 0;
-    protected int capacity = 0;
+public class ArrayList{
+    protected Entry[] data;
+    protected int size;
+    protected int capacity;
 
-    //public ArrayList() { this(CAPACITY); }
+    public ArrayList() {
+    	this.size = 0;
+    	this.capacity = 0;
+    }
 
+    //initialize data array of type E[] and set the capacity
     public ArrayList(int capacity) {
-       data = (E[]) new Object[capacity];
+       super();
+       data = new Entry[capacity];
        this.capacity = capacity;
     }
 
@@ -20,20 +25,23 @@ public class ArrayList<E> {
         return size == 0;
     }
 
-    public E get(int i) throws IndexOutOfBoundsException {
+    //check if index valid, then return element at index i
+    public Entry get(int i) throws IndexOutOfBoundsException {
     	//System.out.println("get element");
     	checkIndex(i, size);
         return data[i];
     }
 
-    public E set(int i, E e) throws IndexOutOfBoundsException {
+    //check if index valid, then replace old element with new element, finally return old element
+    public Entry set(int i, Entry e) throws IndexOutOfBoundsException {
     	checkIndex(i, size);
-    	E replaced = data[i];
+    	Entry replaced = data[i];
     	data[i] = e;
     	return replaced;
     }
 
-    public void add(int i, E e) throws IndexOutOfBoundsException, IllegalStateException {
+    //check if index is valid & array is not full, then shift elements right starting from right to curr index
+    public void add(int i, Entry e) throws IndexOutOfBoundsException, IllegalStateException {
     	//System.out.println("add element");
     	checkIndex(i, size + 1); 
     	if (size == data.length) throw new IllegalStateException("Array is full");
@@ -43,10 +51,10 @@ public class ArrayList<E> {
 		size++;
     }
 
-    public E remove(int i) throws IndexOutOfBoundsException {
+    public Entry remove(int i) throws IndexOutOfBoundsException {
     	//System.out.println("remove element");
     	checkIndex(i, size);
-    	E removed = data[i];
+    	Entry removed = data[i];
     	for (int j = i; j < size - 1; j++) { // shift down for a removal at index i 
     		data[j] = data[j+1];
     	}
@@ -55,9 +63,32 @@ public class ArrayList<E> {
         return removed; 
     }
    
+    //check if index is within array bounds
     protected void checkIndex(int i, int n) throws IndexOutOfBoundsException {
     	//System.out.println("check index: i = " + i + " n = " + n);
         if (i < 0 || i >= n) throw new IndexOutOfBoundsException("Illegal index: " + i);
+    }
+    
+    //nested Entry class
+    protected class Entry {
+        
+        private String k;  // key
+        private String v;  // value
+
+        public Entry(String key, String value) {
+          this.k = key;
+          this.v = value;
+        }
+
+        // methods of the Entry interface
+        public String getKey() { return k; }
+        public String getValue() { return v; }
+
+        // utilities not exposed as part of the Entry interface
+        protected void setKey(String key) { k = key; }
+        protected void setValue(String value) { v = value; }
+      
+      
     }
 
 }
