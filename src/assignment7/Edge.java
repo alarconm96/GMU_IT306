b88ec4607ae3b1343cdbd8d8de374bcce617d4ef
Marco Alarcon <alarconm96@gmail.com>
2022-04-25 17:29:40 -0400

    Create Assignment7 package with initial files

---
+++ src/assignment7/Edge.java
@@ -0,0 +1,10 @@
+package assignment7;
+
+
+/**
+ * An edge of a graph.
+ **/
+public interface Edge<E> {
+  /** Returns the element associated with the edge. */
+	E getElement();
+}
---
+++ src/assignment7/GraphExamples.java
@@ -0,0 +1,65 @@
+package assignment7;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * This class provides a utility to build a graph from a list of edges.
+ *
+
+ */
+public class GraphExamples {
+
+  /**
+   * Constructs a graph from an array of array strings.
+   *
+   * An edge can be specified as { "SFO", "LAX" }, in which case edge is created
+   * with default edge value of 1, or as { "SFO", "LAX", "337" }, in which case
+      labels.add(edge[0]);
+   * the third entry should be a string that will be converted to an integral value.
+   */
+  public static Graph<String,Integer> graphFromEdgelist(String[][] edges, boolean directed) {
+    Graph<String,Integer> g = new AdjacencyMapGraph<>(directed);
+
+    // first pass to get sorted set of vertex labels
+    TreeSet<String> labels = new TreeSet<>();
+    for (String[] edge : edges) {
+      labels.add(edge[0]);
+      labels.add(edge[1]);
+    }
+
+    // now create vertices (in alphabetical order)
+    HashMap<String, Vertex<String> > verts = new HashMap<>();
+    for (String label : labels)
+      verts.put(label, g.insertVertex(label));
+    
+
+    // now add edges to the graph
+    for (String[] edge : edges) {
+      Integer cost = (edge.length == 2 ? 1 : Integer.parseInt(edge[2]));
+      g.insertEdge(verts.get(edge[0]), verts.get(edge[1]), cost);
+    }
+    return g;
+  }
+
+  /** Returns the unweighted, directed graph from Figure 14.12 of DSAJ6. */
+  
+  
+  public static Graph<String,Integer> Graph0() {
+    String[][] edges = {
+      {"A","C"}, {"A","D"}, {"B","D"}, {"B", "F"}, {"C","D"}, {"C","E"},
+      {"D","F"}, {"E","G"}, {"F","G"}, {"F","H"}, {"G","H"}
+    };
+    return graphFromEdgelist(edges, true);
+  }
+
+
+  public static void main(String[] args) {
+  
+    System.out.println("Graph 0");
+    System.out.println(Graph0());
+
+  }
+
+}
